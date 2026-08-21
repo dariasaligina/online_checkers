b3d0a625e7630a7a97cc3447f38768b33a34f47a
dariasaligina <146203741+dariasaligina@users.noreply.github.com>
2026-08-21 15:46:25 +0300

    функция NewSquare

--- checkers.go
+++ checkers.go
@@ -1,7 +1,23 @@
 package checkers
 
+import "errors"
+
 type board [8][8]rune
 
 type сoordinate [2]uint8
 
 type square string
+
+func NewSquare(s string) (square, error) {
+	if len(s) != 2 {
+		return square(""), errors.New("Invalid coordinate format. Expected 2 characters (e.g., 'e4').")
+	}
+	if s[0]<'a' || s[0]>'h'{
+		return square(""), errors.New("Invalid column. Use letters from 'a' to 'h'.")
+	}
+	if s[1]<'1' || s[1]>'8'{
+		return square(""), errors.New("Invalid row. Use numbers from '1' to '8'")
+	}
+	return square(s), nil
+}
+
