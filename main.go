8d1c9932d5adaab0acbcc6901eceeb4dc7937dfe
dariasaligina <146203741+dariasaligina@users.noreply.github.com>
2026-08-31 16:35:27 +0300

    вычисление простых ходов для дамок

--- main.go
+++ main.go
@@ -9,4 +9,10 @@ func main() {
 	board := checkers.NewBoard()
 	fmt.Println(board)
 	fmt.Println(len(board.String()))
+	s1, _ := checkers.NewSquare("a3")
+	s2, _ := checkers.NewSquare("b6")
+	s3, _ := checkers.NewSquare("e3")
+	fmt.Println(board.SimpleMoves(s1))
+	fmt.Println(board.SimpleMoves(s2))
+	fmt.Println(board.SimpleMoves(s3))
 }
